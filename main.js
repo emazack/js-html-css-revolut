cfbd367e099cdf0d1744e7dc2b30ab0a918a4c94
Emanuele <em.zaccaria@gmail.com>
2020-04-01 17:32:56 +0200

    JS prova struttura js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-document.ready(function(){
+$(document).ready(function(){
   // prendo l'elemento bar hamburger da cliccare
   var barHamburger = $(".bar-hamburger");
   // prendo l'elemento su cui eseguire l'azione, menu Hamburger
   var menuHamburger = $(".menu-hambuger")
   // quando lo clicco deve accadere qualcosa
   barHamburger.click( function(){
-    $(this).toggle()
+    menuHamburger.show()
   })
-
-
-
-
-
 });
