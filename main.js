24b8bdba55fce5c4d0634f9440513b5779c09780
Emanuele <em.zaccaria@gmail.com>
2020-04-01 17:55:27 +0200

    JS primo hamburger funziona

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   // prendo l'elemento bar hamburger da cliccare
   var barHamburger = $(".bar-hamburger");
   // prendo l'elemento su cui eseguire l'azione, menu Hamburger
-  var menuHamburger = $(".menu-hambuger")
+  var menuHamburger = $(".menu-hamburger");
   // quando lo clicco deve accadere qualcosa
   barHamburger.click( function(){
-    menuHamburger.show()
+    menuHamburger.toggle();
   })
 });
