40a9ff3f937d1bd0a5aaf11ac4f98fdc1a962595
Emanuele <em.zaccaria@gmail.com>
2020-04-01 17:20:00 +0200

    HTML e CSS sistemazione generale delle classi

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 document.ready(function(){
-  
+  // prendo l'elemento bar hamburger da cliccare
+  var barHamburger = $(".bar-hamburger");
+  // prendo l'elemento su cui eseguire l'azione, menu Hamburger
+  var menuHamburger = $(".menu-hambuger")
+  // quando lo clicco deve accadere qualcosa
+  barHamburger.click( function(){
+    $(this).toggle()
+  })
 
 
 
